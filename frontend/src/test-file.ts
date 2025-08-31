b2a7ac6af08398e52285ba1ee48d5b38e3d6e80e
Gregory Foo <gregoryfoo@hotmail.com>
2025-08-31 22:37:16 +0800

    feat: Complete pre-commit hook setup
    - Update lint-staged configuration to remove deprecated git add
    - Successfully tested pre-commit hook prevents commits with linting errors
    - ESLint finds 102 issues in current codebase (78 errors, 24 warnings)
    - Pre-commit hook will automatically fix fixable issues and block commits with errors

--- frontend/src/test-file.ts
+++ frontend/src/test-file.ts
@@ -1,6 +1,7 @@
 // Test file with linting errors
 var unused_variable = "This should cause linting errors"
 console.log("This should warn")
+let another_var = "more errors"
 
 export const testFunction = () => {
   const unusedVar = "unused"
