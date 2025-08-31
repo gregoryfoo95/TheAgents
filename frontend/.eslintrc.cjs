37d75e26775e4d28ce8b0d229f8b56c36d1d4d54
Gregory Foo <gregoryfoo@hotmail.com>
2025-08-31 22:36:04 +0800

    feat: Set up ESLint configuration and pre-commit hooks for frontend
    - Add comprehensive ESLint configuration for React + TypeScript
    - Include React hooks linting rules
    - Add husky and lint-staged for pre-commit linting
    - Add lint:fix script for automatic fixing

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-refresh', 'react-hooks'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-console': 'warn',
+    'eqeqeq': 'error',
+    'no-duplicate-imports': 'error',
+    'no-unused-vars': 'off', // Turn off base rule as we use @typescript-eslint version
+  },
+}
